b57f2b0614a447fa2d3ee88b4c3aa293a00904ea
Jay Henderson <jjhenderson@gmail.com>
2017-04-03 00:41:22 -0500

    Implement functionality to create new cards via http request

--- scrumboard/static/scrumboard/scrumboard.js
+++ scrumboard/static/scrumboard/scrumboard.js
@@ -5,22 +5,32 @@
             'use strict';
 
             angular.module('scrumboard.demo', [])
-                .controller('ScrumboardController', [ '$scope', '$http', ScrumboardController]);
+                .controller('ScrumboardController',
+                    [ '$scope', '$http', ScrumboardController]);
 
             function ScrumboardController($scope, $http) {
                 $scope.add = function (list, title) {
                     var card = {
+                        list_id: list.id,
                         title: title
                     };
-
-                    list.cards.push(card);
+                    $http.post('/scrumboard/cards/',card)
+                        .then(function(response){
+                            list.cards.push(response.data);
+                        },
+                        function (){
+                            alert('Could not create card');
+                        });
                 };
 
                 $scope.data = [];
-                $http.get('/scrumboard/lists').then(function(response){
+                $http.get('/scrumboard/lists/').then(
+                    function(response){
                     $scope.data = response.data;
-                });
+                }
 
-            }
+            );
 
-        }());
+        }
+
+    }());
